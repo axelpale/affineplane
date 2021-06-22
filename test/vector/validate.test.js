c28e39df3cf8bc5d8011fbebe05273816cafdeef
Akseli Palen <akseli.palen@gmail.com>
2021-06-22 20:57:05 +0300

    vector.validate with tests

---
+++ test/vector/validate.test.js
@@ -0,0 +1,20 @@
+const vector = require('../../lib/vector')
+
+module.exports = (ts) => {
+  ts.test('case: valid vector', (t) => {
+    t.ok(vector.validate({ x: 0, y: 1 }))
+    t.ok(vector.validate({ x: 0, y: 1, z: 2 }), 'allow additional props')
+
+    t.end()
+  })
+
+  ts.test('case: invalid vector', (t) => {
+    t.notOk(vector.validate(null), 'detect null')
+    t.notOk(vector.validate({}), 'detect empty object')
+    t.notOk(vector.validate({ x: 0 }), 'detect missing prop')
+    t.notOk(vector.validate({ x: 0, y: '1' }), 'detect string value')
+    t.notOk(vector.validate({ x: 0, y: NaN }), 'detect NaN')
+
+    t.end()
+  })
+}
