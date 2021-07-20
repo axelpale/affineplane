185cfe82dfa28a4826f4d096effe837977202729
Akseli Palen <akseli.palen@gmail.com>
2021-07-20 21:46:36 +0300

    Complete module index

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 
-exports.EPSILON = require('./lib/epsilon')
+exports.basis = require('./lib/basis')
 
+exports.distance = require('./lib/distance')
+
+exports.epsilon = require('./lib/epsilon')
+
+exports.formation = require('./lib/formation')
 
 exports.point = require('./lib/point')
+
+exports.point3r = require('./lib/point3r')
+
+exports.rectangle = require('./lib/rectangle')
+
+exports.size = require('./lib/size')
 
 exports.transform = require('./lib/transform')
 
 exports.vector = require('./lib/vector')
+
+exports.vector3r = require('./lib/vector3r')
 
 exports.version = require('./lib/version')
