9350153bd7e3fc330e425b1e549c852f9a2aab40
Akseli Palen <akseli.palen@gmail.com>
2022-04-15 15:37:51 +0300

    add custom gendocs keywords: linear2, proj2, tran2, vector2

--- doc/gendocs/prettyText.js
+++ doc/gendocs/prettyText.js
@@ -15,13 +15,19 @@ const removeGenversion = (text) => {
   return text.replace(expressions.genversion, '')
 }
 
+const linear2Keyword = /linear2($|\W+)/g
 const pointKeyword = /(a|optional|of) point(s?)($|\W+)/g
-const transformKeyword = /tran2($|\W+)/g
+const proj2Keyword = /proj2($|\W+)/g
+const tran2Keyword = /tran2($|\W+)/g
+const vector2Keyword = /vector2($|\W+)/g
 const moduleKeyword = /affineplane\.(\w+)($|\.$|\.\W+| )/g
 const memberKeyword = /affineplane\.(\w+)\.(\w+)($|\W+| )/g
 const routeKeywords = (text) => {
+  text = text.replace(linear2Keyword, '[linear2](#affineplanelinear2)$1')
   text = text.replace(pointKeyword, '$1 [point$2](#nudgedpoint)$3')
-  text = text.replace(transformKeyword, '[tran2](#affineplanetran2)$1')
+  text = text.replace(proj2Keyword, '[proj2](#affineplaneproj2)$1')
+  text = text.replace(tran2Keyword, '[tran2](#affineplanetran2)$1')
+  text = text.replace(vector2Keyword, '[vector2](#affineplanevector2)$1')
   text = text.replace(moduleKeyword, '[affineplane.$1](#affineplane$1)$2')
   text = text.replace(memberKeyword, '[affineplane.$1.$2](#affineplane$1$2)$3')
   return text
