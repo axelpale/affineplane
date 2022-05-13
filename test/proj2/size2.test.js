727bc8401e4a0ac61fe33b9fae05d5c066d35c01
Akseli Palen <akseli.palen@gmail.com>
2022-05-13 18:07:17 +0300

    grow proj2.size2 test suite

--- test/proj2/size2.test.js
+++ test/proj2/size2.test.js
@@ -12,4 +12,15 @@ module.exports = (ts) => {
 
     t.end()
   })
+
+  ts.test('case: change under scaling', (t) => {
+    // Create a projection. The target plane double the scale.
+    const pr = affineplane.tran2.X2
+    // The rotation does not affect
+    t.deepEqual(proj2.size2(pr, { w: 0, h: 0 }), { w: 0, h: 0 }, 'zero size')
+    t.deepEqual(proj2.size2(pr, { w: 2, h: 2 }), { w: 1, h: 1 }, 'half size')
+    t.deepEqual(proj2.size2(pr, { w: 4, h: 2 }), { w: 2, h: 1 }, 'half size')
+
+    t.end()
+  })
 }
