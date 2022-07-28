55f2d76addeef25d7b79997fc351eec8b7bb8681
Akseli Palen <akseli.palen@gmail.com>
2022-07-29 00:59:00 +0300

    init proj3 test suite

---
+++ test/proj3/index.test.js
@@ -0,0 +1,10 @@
+// A unit for each method.
+const units = {
+  compose: require('./compose.test'),
+}
+
+module.exports = (t) => {
+  Object.keys(units).forEach((unitName) => {
+    t.test('affineplane.proj3.' + unitName, units[unitName])
+  })
+}
