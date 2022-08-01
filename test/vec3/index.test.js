3d1d162e2328ebee639f69a370b0260ee3ecd452
Akseli Palén <akseli.palen@gmail.com>
2022-08-01 18:32:27 +0300

    impl cylindrical vec3.toPolar

--- test/vec3/index.test.js
+++ test/vec3/index.test.js
@@ -1,12 +1,13 @@
 // A unit for each method.
 const units = {
-  fromPolar: require('./fromSpherical.test'),
+  fromSpherical: require('./fromSpherical.test'),
   inverse: require('./inverse.test'),
   magnitude: require('./magnitude.test'),
   rotateBy: require('./rotateBy.test'),
   rotateTo: require('./rotateTo.test'),
   sum: require('./sum.test'),
-  toPolar: require('./toSpherical.test')
+  toPolar: require('./toPolar.test'),
+  toSpherical: require('./toSpherical.test')
 }
 
 module.exports = (t) => {
