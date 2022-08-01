fa2b577153b172ad9e76a4f606ab07056ddc494c
Akseli Palén <akseli.palen@gmail.com>
2022-08-01 23:17:16 +0300

    impl helm3.almostEqual

---
+++ test/helm3/almostEqual.test.js
@@ -0,0 +1,32 @@
+const helm3 = require('../../lib/helm3')
+
+module.exports = (ts) => {
+  ts.test('case: basic equal', (t) => {
+
+    t.true(helm3.almostEqual(
+      { a: 0, b: 0, x: 0, y: 0, z: 0 },
+      { a: 0, b: 0, x: 0, y: 0, z: 0 },
+      0
+    ), 'zero tolerance')
+
+    t.false(helm3.almostEqual(
+      { a: 0, b: 0, x: 0, y: 0, z: 0 },
+      { a: 0, b: 0, x: 0, y: 0, z: 0 },
+      -1
+    ), 'negative tolerance')
+
+    t.true(helm3.almostEqual(
+      { a: 1, b: 0, x: 0, y: 0, z: 0 },
+      { a: 0, b: 0, x: 0, y: 0, z: 0 },
+      1
+    ), 'inclusive tolerance')
+
+    t.false(helm3.almostEqual(
+      { a: 1, b: 0, x: 0, y: 0, z: 0 },
+      { a: 0, b: 0, x: 0, y: 0, z: 0 },
+      0.5
+    ), 'out of tolerance')
+
+    t.end()
+  })
+}
