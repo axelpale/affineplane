4199f3967bf9a4452cec62c2c03dfb4bb5cc2d71
Akseli Palén <akseli.palen@gmail.com>
2022-08-01 18:17:57 +0300

    rename vec3 polar to spherical

--- test/vec3/fromSpherical.test.js
+++ test/vec3/fromSpherical.test.js
@@ -4,7 +4,7 @@ const PI = Math.PI
 module.exports = (ts) => {
   ts.test('case: basic polar coords', (t) => {
     t.almostEqual(
-      vec3.fromPolar(2, PI / 2, PI / 2),
+      vec3.fromSpherical(2, PI / 2, PI / 2),
       { x: 0, y: 0, z: 2 }
     )
 
@@ -13,7 +13,7 @@ module.exports = (ts) => {
 
   ts.test('case: unset pitch', (t) => {
     t.almostEqual(
-      vec3.fromPolar(2, PI / 2),
+      vec3.fromSpherical(2, PI / 2),
       { x: 0, y: 2, z: 0 }
     )
 
