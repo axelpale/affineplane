d0c466f05b449e090d1821dd577caafaa3d6044b
Akseli Palén <akseli.palen@gmail.com>
2022-08-02 15:03:16 +0300

    improve almostEqual to understand helm transforms

--- test/utils/almost.js
+++ test/utils/almost.js
@@ -1,8 +1,8 @@
 const EPSILON = require('../../lib/epsilon')
 const vec2 = require('../../lib/vec2')
 const vec3 = require('../../lib/vec3')
-const vec2AlmostEqual = vec2.almostEqual
-const vec3AlmostEqual = vec3.almostEqual
+const helm2 = require('../../lib/helm2')
+const helm3 = require('../../lib/helm3')
 
 module.exports = function (actual, expected) {
   // Detects almost equality of numbers, points, and vectors.
@@ -16,9 +16,17 @@ module.exports = function (actual, expected) {
   } else if (type === 'object') {
     if ('x' in expected && 'y' in expected) {
       if ('z' in expected) {
-        assertion = vec3AlmostEqual(actual, expected)
+        if ('a' in expected && 'b' in expected) {
+          assertion = helm3.almostEqual(actual, expected)
+        } else {
+          assertion = vec3.almostEqual(actual, expected)
+        }
       } else {
-        assertion = vec2AlmostEqual(actual, expected)
+        if ('a' in expected && 'b' in expected) {
+          assertion = helm2.almostEqual(actual, expected)
+        } else {
+          assertion = vec2.almostEqual(actual, expected)
+        }
       }
     } else {
       assertion = actual === expected
