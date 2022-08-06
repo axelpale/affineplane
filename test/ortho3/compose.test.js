204174ec8aae8d67eb6e45ab1d96c46845a335f1
Akseli Palén <akseli.palen@gmail.com>
2022-08-07 01:26:22 +0300

    test ortho3.compose

--- test/ortho3/compose.test.js
+++ test/ortho3/compose.test.js
@@ -4,30 +4,43 @@ const ortho3 = affineplane.ortho3
 module.exports = (ts) => {
   ts.test('case: basic compose', (t) => {
     // Create a projection. The target plane is rotated 90 deg CW
-    const pra = affineplane.helm3.from({
-      scale: 1,
-      angle: Math.PI,
-      tx: 100,
-      ty: 1000,
-      tz: 10
+    const pra = ortho3.fromFeats({
+      scaling: 1,
+      rotation: Math.PI,
+      translation: {
+        x: 0,
+        y: 1,
+        z: 1
+      }
     })
-    const prb = affineplane.helm3.from({
-      scale: 2,
-      angle: Math.PI,
-      tx: 50,
-      ty: 2000,
-      tz: -20
+    const prb = ortho3.fromFeats({
+      scaling: 2,
+      rotation: Math.PI,
+      translation: {
+        x: 1,
+        y: 2,
+        z: -2
+      }
     })
 
-    const prc = ortho3.compose(pra, prb)
+    // let v be a vector.
+    // prb is applied to v first to get vb.
+    // then pra is applied to vb to get vab.
+    // The result should be same when prc
+    // is applied to v.
+    const prc = ortho3.fromFeats({
+      scaling: 2,
+      rotation: 0,
+      translation: {
+        x: -1,
+        y: -1,
+        z: -1
+      }
+    })
 
-    t.ok(ortho3.almostEqual(prc, {
-      scale: 2,
-      angle: 0,
-      tx: 150,
-      ty: 3000,
-      tz: -10
-    }), 'ortho3 almost equal')
+    const prcHat = ortho3.compose(pra, prb)
+
+    t.almostEqual(prcHat, prc, 'ortho3 almost equal')
 
     t.end()
   })
