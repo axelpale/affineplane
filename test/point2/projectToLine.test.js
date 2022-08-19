9d841a5dadd30d304c87dde190b90450cba8dd82
Akseli Palén <akseli.palen@gmail.com>
2022-08-19 12:36:48 +0300

    rename point2.project to .projectToLine

--- test/point2/projectToLine.test.js
+++ test/point2/projectToLine.test.js
@@ -2,12 +2,12 @@ const point2 = require('../../lib/point2')
 const line2 = require('../../lib/line2')
 
 module.exports = (ts) => {
-  ts.test('case: basic project', (t) => {
+  ts.test('case: basic projection to line', (t) => {
     const origin = { x: 0, y: 0 }
     const span = { x: 1, y: 0 }
     const line = line2.create(origin, span)
     t.deepEqual(
-      point2.project({ x: 3, y: 1 }, line),
+      point2.projectToLine({ x: 3, y: 1 }, line),
       { x: 3, y: 0 }
     )
 
@@ -19,7 +19,7 @@ module.exports = (ts) => {
     const span = { x: -1, y: -1 }
     const line = line2.create(origin, span)
     t.deepEqual(
-      point2.project({ x: 3, y: -3 }, line),
+      point2.projectToLine({ x: 3, y: -3 }, line),
       { x: 0, y: 0 }
     )
 
