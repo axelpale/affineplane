b33c532377bb01635423eea51e22fcc4bb7fa360
Akseli Palén <akseli.palen@gmail.com>
2022-08-20 21:16:05 +0300

    update spiral example

--- examples/spiral/app.js
+++ examples/spiral/app.js
@@ -1,6 +1,10 @@
-// Affine geometry example
+// Affine geometry example.
+//
+// Here we give an example for point2 and herm2 geometries.
+//
+//
 
-const plane = require('../../index')
+const affineplane = require('../../index')
 
 // Setup
 
@@ -11,18 +15,9 @@ const canvas = document.querySelector('canvas')
 // Drawing tools
 
 const drawPoint = (ctx, p) => {
+  // Draw point on canvas
   ctx.fillStyle = 'white'
   ctx.fillRect(p.x - 5, p.y - 5, 10, 10)
-}
-
-const drawRandomPoint = (ctx, x, y, w, h) => {
-  const rx = x + Math.random() * w
-  const ry = y + Math.random() * h
-
-  drawPoint(ctx, {
-    x: rx,
-    y: ry
-  })
 }
 
 const drawAll = (ctx) => {
@@ -34,14 +29,15 @@ const drawAll = (ctx) => {
     y: Math.round(h / 2)
   }
 
-  let p = plane.point.offset(c, 5, 0)
+  let p = affineplane.point2.offset(c, 5, 0)
 
-  let tr = plane.transform.I
-  tr = plane.transform.rotateBy(tr, c, Math.PI / 6)
-  tr = plane.transform.scaleBy(tr, c, Math.pow(1.618033, 1 / 5))
+  // Create a helmert transformation
+  let tr = affineplane.helm2.I
+  tr = affineplane.helm2.rotateBy(tr, c, Math.PI / 6)
+  tr = affineplane.helm2.scaleBy(tr, c, Math.pow(1.618033, 1 / 5))
 
   for (let i = 0; i < 200; i += 1) {
-    p = plane.point.transform(p, tr)
+    p = affineplane.point2.transform(p, tr)
     drawPoint(ctx, p)
   }
 }
--- examples/spiral/webpack.config.js
+++ examples/spiral/webpack.config.js
@@ -1,12 +1,9 @@
-const webpack = require('webpack')
-const path = require('path')
-
 module.exports = {
   entry: './app.js',
   output: {
     filename: 'app.bundle.js',
     path: __dirname,
-    sourceMapFilename: '[file].map',
+    sourceMapFilename: '[file].map'
   },
 
   devtool: 'source-map',
