6cfcdb5a85eb0c77dad1d4996358ee14439cf8ac
Akseli Palén <akseli.palen@gmail.com>
2022-08-23 13:17:42 +0300

    drop helm3 perspective projection as non applicable

--- test/helm3/projectToPlane.test.js
+++ test/helm3/projectToPlane.test.js
@@ -19,33 +19,4 @@ module.exports = (ts) => {
 
     t.end()
   })
-
-  ts.test('case: basic perspective projection to plane', (t) => {
-    const plane = { a: 1, b: 0, x: 0, y: 0, z: 0 }
-    const camera = { x: 1, y: 1, z: 0 }
-    t.deepEqual(
-      helm3.projectToPlane({ a: 2, b: 0, x: 3, y: 0, z: 0 }, plane, camera),
-      { a: 0, b: 0, x: 1, y: 1 },
-      'singular cuz camera on geometry'
-    )
-
-    const planeb = { a: 1, b: 0, x: 0, y: 0, z: -2 }
-    const camerab = { x: 0, y: 0, z: -4 }
-    t.deepEqual(
-      helm3.projectToPlane({ a: 2, b: 0, x: 2, y: 0, z: 0 }, planeb, camerab),
-      { a: 1, b: 0, x: 1, y: 0 },
-      'effect to half due to perspect'
-    )
-
-    const planec = { a: 3, b: 0, x: 0, y: 0, z: -2 }
-    const camerac = { x: 0, y: 0, z: -4 }
-    t.deepEqual(
-      helm3.projectToPlane({ a: 6, b: 0, x: 6, y: 0, z: 4 }, planec, camerac),
-      { a: 0.5, b: 0, x: 0.5, y: 0 },
-      'scaled projection plane'
-    )
-
-    t.end()
-  })
-
 }
