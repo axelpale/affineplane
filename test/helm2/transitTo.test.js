369faadf51b09a4ffa47903250de651e3950de22
Akseli Palén <akseli.palen@gmail.com>
2022-08-24 15:44:00 +0300

    distinguish helm2 and plane2 transitions

--- test/helm2/transitTo.test.js
+++ test/helm2/transitTo.test.js
@@ -2,11 +2,22 @@ const helm2 = require('../../lib/helm2')
 
 module.exports = (ts) => {
   ts.test('case: basic helm2 transit to plane', (t) => {
-    const plane = { a: 2, b: 0, x: 20, y: 30 }
+    let tr, plane
 
+    tr = { a: 2, b: 0, x: 20, y: 30 }
+    plane = { a: 2, b: 0, x: 20, y: 30 }
     t.deepEqual(
-      helm2.transitTo({ a: 2, b: 0, x: 20, y: 30 }, plane),
-      { a: 1, b: 0, x: 0, y: 0 }
+      helm2.transitTo(tr, plane),
+      { a: 2, b: 0, x: 10, y: 15 },
+      'preserve scaling effect'
+    )
+
+    tr = { a: 1, b: 0, x: 10, y: 20 }
+    plane = { a: 0, b: 2, x: 0, y: 0 }
+    t.deepEqual(
+      helm2.transitTo(tr, plane),
+      { a: 1, b: 0, x: 10, y: -5 },
+      'preserve rotating effect'
     )
 
     t.end()
