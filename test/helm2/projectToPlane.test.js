2bfe32679a69fe2161929f0981e7f08d7b37904a
Akseli Palén <akseli.palen@gmail.com>
2022-08-24 15:57:58 +0300

    rewrite plane2 and plane3 projectToPlane

--- test/helm2/projectToPlane.test.js
+++ test/helm2/projectToPlane.test.js
@@ -1,51 +1,25 @@
 const helm2 = require('../../lib/helm2')
-const plane3 = require('../../lib/plane3')
 
 module.exports = (ts) => {
   ts.test('case: basic orthogonal projection to plane', (t) => {
-    const plane = { a: 1, b: 0, x: 0, y: 0, z: 0 }
+    let tr, target
+
+    tr = { a: 2, b: 0, x: 3, y: 0 }
+    target = { a: 1, b: 0, x: 0, y: 0, z: 0 }
     t.deepEqual(
-      helm2.projectToPlane({ a: 2, b: 0, x: 3, y: 0 }, plane),
+      helm2.projectToPlane(tr, target),
       { a: 2, b: 0, x: 3, y: 0 },
-      'trivial'
+      'trivial plane'
     )
 
-    const planeb = { a: 2, b: 0, x: 1, y: 2, z: 0 }
+    tr = { a: 2, b: 0, x: 3, y: 0 }
+    target = { a: 2, b: 0, x: 1, y: 2, z: 0 }
     t.deepEqual(
-      helm2.projectToPlane({ a: 2, b: 0, x: 3, y: 0 }, planeb),
-      { a: 1, b: 0, x: 1, y: -1 },
-      'project tr to plane orthogonally'
+      helm2.projectToPlane(tr, target),
+      { a: 2, b: 0, x: 1.5, y: 0 },
+      'helm does not lose scaling effect but translation changes'
     )
 
     t.end()
   })
-
-  ts.test('case: basic perspective projection to plane', (t) => {
-    const plane = { a: 1, b: 0, x: 0, y: 0, z: 0 }
-    const camera = { x: 1, y: 1, z: 0 }
-    t.deepEqual(
-      helm2.projectToPlane({ a: 2, b: 0, x: 3, y: 0 }, plane, camera),
-      { a: 0, b: 0, x: 1, y: 1 },
-      'singular cuz camera on geometry'
-    )
-
-    const planeb = { a: 1, b: 0, x: 0, y: 0, z: -2 }
-    const camerab = { x: 0, y: 0, z: -4 }
-    t.deepEqual(
-      helm2.projectToPlane({ a: 2, b: 0, x: 2, y: 0 }, planeb, camerab),
-      { a: 1, b: 0, x: 1, y: 0 },
-      'effect to half due to perspect'
-    )
-
-    const planec = { a: 3, b: 0, x: 0, y: 0, z: -2 }
-    const camerac = { x: 0, y: 0, z: -4 }
-    t.deepEqual(
-      helm2.projectToPlane({ a: 6, b: 0, x: 6, y: 0 }, planec, camerac),
-      { a: 1, b: 0, x: 1, y: 0 },
-      'scaled projection plane'
-    )
-
-    t.end()
-  })
-
 }
--- test/helm3/projectToPlane.test.js
+++ test/helm3/projectToPlane.test.js
@@ -1,20 +1,23 @@
 const helm3 = require('../../lib/helm3')
-const plane3 = require('../../lib/plane3')
 
 module.exports = (ts) => {
   ts.test('case: basic orthogonal projection to plane', (t) => {
-    const plane = { a: 1, b: 0, x: 0, y: 0, z: 0 }
+    let tr, target
+
+    tr = { a: 2, b: 0, x: 3, y: 0, z: 2 }
+    target = { a: 1, b: 0, x: 0, y: 0, z: 0 }
     t.deepEqual(
-      helm3.projectToPlane({ a: 2, b: 0, x: 3, y: 0, z: 2 }, plane),
+      helm3.projectToPlane(tr, target),
       { a: 2, b: 0, x: 3, y: 0 },
-      'trivial'
+      'trivial plane, lose z'
     )
 
-    const planeb = { a: 2, b: 0, x: 1, y: 2, z: 0 }
+    tr = { a: 2, b: 0, x: 3, y: 0, z: 2 }
+    target = { a: 2, b: 0, x: 1, y: 2, z: 0 }
     t.deepEqual(
-      helm3.projectToPlane({ a: 2, b: 0, x: 3, y: 0, z: 2 }, planeb),
-      { a: 1, b: 0, x: 1, y: -1 },
-      'project tr to plane orthogonally'
+      helm3.projectToPlane(tr, target),
+      { a: 2, b: 0, x: 1.5, y: 0 },
+      'preserve scaling effect'
     )
 
     t.end()
