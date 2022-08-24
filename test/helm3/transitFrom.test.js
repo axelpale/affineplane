2fab1d2a2b3d666065b13bb1c533b2cbfee861d0
Akseli Palén <akseli.palen@gmail.com>
2022-08-24 15:56:01 +0300

    distinguish helm3 and plane3 transitions

--- test/helm3/transitFrom.test.js
+++ test/helm3/transitFrom.test.js
@@ -2,11 +2,14 @@ const helm3 = require('../../lib/helm3')
 
 module.exports = (ts) => {
   ts.test('case: basic helm3 transit from plane', (t) => {
-    const plane = { a: 1, b: 0, x: 2, y: 3, z: 4 }
+    let tr, source
 
+    tr = { a: 2, b: 0, x: 2, y: 3, z: 5 }
+    source = { a: 1, b: 0, x: 2, y: 4, z: 6 }
     t.deepEqual(
-      helm3.transitFrom({ a: 2, b: 0, x: 2, y: 3, z: 5 }, plane),
-      { a: 2, b: 0, x: 4, y: 6, z: 9 }
+      helm3.transitFrom(tr, source),
+      { a: 2, b: 0, x: 2, y: 3, z: 5 },
+      'plane translation does not affect'
     )
 
     t.end()
--- test/helm3/transitTo.test.js
+++ test/helm3/transitTo.test.js
@@ -2,11 +2,14 @@ const helm3 = require('../../lib/helm3')
 
 module.exports = (ts) => {
   ts.test('case: basic helm3 transit to plane', (t) => {
-    const plane = { a: 2, b: 0, x: 20, y: 30, z: 40 }
+    let tr, target
 
+    tr = { a: 2, b: 0, x: 20, y: 30, z: 40 }
+    target = { a: 2, b: 0, x: 20, y: 30, z: 40 }
     t.deepEqual(
-      helm3.transitTo({ a: 2, b: 0, x: 20, y: 30, z: 40 }, plane),
-      { a: 1, b: 0, x: 0, y: 0, z: 0 }
+      helm3.transitTo(tr, target),
+      { a: 2, b: 0, x: 10, y: 15, z: 40 },
+      'preserve depth displacement'
     )
 
     t.end()
