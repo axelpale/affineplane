a7e9ef72e8dcb5865b77515cca523db18f00a5e7
Akseli Palén <akseli.palen@gmail.com>
2022-08-25 14:14:39 +0300

    impl plane2 rotateTo

--- test/plane2/rotateTo.test.js
+++ test/plane2/rotateTo.test.js
@@ -1,11 +1,7 @@
-// Light-weight tests because
-// plane2.rotateTo is helm2.rotateTo
-// which has wider test suite.
-//
 const plane2 = require('../../lib/plane2')
 
 module.exports = (ts) => {
-  ts.test('case: smoke', (t) => {
+  ts.test('case: basic rotation to angle', (t) => {
     let plane, center, angle
 
     plane = { a: -1, b: 0, x: 0, y: 0 }
@@ -15,6 +11,15 @@ module.exports = (ts) => {
       plane2.rotateTo(plane, center, angle),
       { a: 0, b: 1, x: 0, y: 0 },
       '270 deg rotation to angle'
+    )
+
+    plane = { a: 1, b: 0, x: 0, y: 0 }
+    center = { x: 100, y: 100 }
+    angle = Math.PI / 2
+    t.almostEqual(
+      plane2.rotateTo(plane, center, angle),
+      { a: 0, b: 1, x: 200, y: 0 },
+      '90 deg rotation around non-trivial point'
     )
 
     t.end()
