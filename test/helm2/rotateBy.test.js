c2a69fd4d8f68df7fe4e9b0942f7b5a85814ae68
Akseli Palén <akseli.palen@gmail.com>
2022-08-25 13:57:00 +0300

    impl plan2 rotateBy and correct helm2 rotateBy

--- test/helm2/rotateBy.test.js
+++ test/helm2/rotateBy.test.js
@@ -1,19 +1,31 @@
-const helm2 = require('../../index').helm2
-const rotateBy = helm2.rotateBy
-const IDENTITY = helm2.IDENTITY
-const ROT180 = helm2.ROT180
+const helm2 = require('../../lib/helm2')
 
 module.exports = (ts) => {
   ts.test('case: basic rotations', (t) => {
-    const c1 = { x: 0, y: 0 }
-    t.transformEqual(rotateBy(ROT180, c1, Math.PI), IDENTITY)
-    t.transformEqual(rotateBy(IDENTITY, c1, 0), IDENTITY)
+    let helm, angle
 
-    const c2 = { x: 100, y: 100 }
-    t.transformEqual(
-      rotateBy({ a: 1, b: 0, x: 0, y: 0 }, c2, Math.PI),
-      { a: -1, b: 0, x: 200, y: 200 },
-      'around non-trivial center'
+    helm = helm2.ROT180
+    angle = Math.PI
+    t.almostEqual(
+      helm2.rotateBy(helm, angle),
+      helm2.ROT180,
+      'rotation does not affect rotating effect'
+    )
+
+    helm = helm2.IDENTITY
+    angle = 0
+    t.almostEqual(
+      helm2.rotateBy(helm, angle),
+      helm2.IDENTITY,
+      'trivial'
+    )
+
+    helm = { a: 0, b: 2, x: 2, y: 2 }
+    angle = Math.PI / 2
+    t.almostEqual(
+      helm2.rotateBy(helm, angle),
+      { a: 0, b: 2, x: -2, y: 2 },
+      'only translation should be rotated'
     )
 
     t.end()
--- test/plane2/rotateBy.test.js
+++ test/plane2/rotateBy.test.js
@@ -1,8 +1,5 @@
-// Light-weight tests because
-// plane2.rotateBy is helm2.rotateBy
-// which has wider test suite.
-//
 const plane2 = require('../../lib/plane2')
+const helm2 = require('../../lib/helm2')
 
 module.exports = (ts) => {
   ts.test('case: smoke', (t) => {
@@ -14,7 +11,40 @@ module.exports = (ts) => {
     t.almostEqual(
       plane2.rotateBy(plane, center, angle),
       { a: 0, b: 1, x: 0, y: 0 },
-      'rotation'
+      'rotation by 90 deg'
+    )
+
+    t.end()
+  })
+
+  ts.test('case: basic rotations', (t) => {
+    let plane, center, angle
+
+    plane = { a: -1, b: 0, x: 0, y: 0 }
+    center = { x: 0, y: 0 }
+    angle = -Math.PI
+    t.almostEqual(
+      plane2.rotateBy(plane, center, angle),
+      helm2.IDENTITY,
+      'rotation by -180 deg'
+    )
+
+    plane = helm2.IDENTITY
+    center = { x: 0, y: 0 }
+    angle = 0
+    t.almostEqual(
+      plane2.rotateBy(plane, center, angle),
+      helm2.IDENTITY,
+      'rotation by 0 deg'
+    )
+
+    plane = helm2.IDENTITY
+    center = { x: 100, y: 100 }
+    angle = Math.PI
+    t.almostEqual(
+      plane2.rotateBy(plane, center, angle),
+      { a: -1, b: 0, x: 200, y: 200 },
+      'around non-trivial center'
     )
 
     t.end()
