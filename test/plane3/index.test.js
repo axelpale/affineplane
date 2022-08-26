fcab526943e5e066df1226126cd35dc6838a3a45
Akseli Palén <akseli.palen@gmail.com>
2022-08-26 15:16:48 +0300

    impl plane3 compose and transform

--- test/plane3/index.test.js
+++ test/plane3/index.test.js
@@ -1,8 +1,10 @@
 // A unit for each method.
 const units = {
+  compose: require('./compose.test'),
   create: require('./create.test'),
   invert: require('./invert.test'),
   projectToPlane: require('./projectToPlane.test'),
+  transform: require('./transform.test'),
   translateBy: require('./translateBy.test'),
   translateTo: require('./translateTo.test'),
   validate: require('./validate.test')
---
+++ test/plane3/transform.test.js
@@ -0,0 +1,16 @@
+const plane3 = require('../../lib/plane3')
+
+module.exports = (ts) => {
+  ts.test('case: basic transform', (t) => {
+    const plane = { a: 1, b: 0, x: 1, y: 1, z: 1 }
+    const tr = { a: 0, b: 2, x: 2, y: 2, z: 2 }
+
+    t.deepEqual(
+      plane3.transform(plane, tr),
+      { a: 0, b: 2, x: 0, y: 4, z: 3 },
+      'rotate, scale, and translate'
+    )
+
+    t.end()
+  })
+}
