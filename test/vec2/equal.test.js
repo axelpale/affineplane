90c2d98e09323eb1100336ea30e1cb2fadb92e5b
Akseli Palén <akseli.palen@gmail.com>
2022-09-05 14:25:36 +0300

    test vec2 equal invert

--- test/vec2/equal.test.js
+++ test/vec2/equal.test.js
@@ -1,14 +1,24 @@
-module.exports = (p, q) => {
-  // Test if points p,q equal
-  //
-  // Parameters
-  //   p
-  //     a point
-  //   q
-  //     a point
-  //
-  // Return
-  //   boolean
-  //
-  return p.x === q.x && p.y === q.y
+const affineplane = require('../../index')
+const vec2 = affineplane.vec2
+
+module.exports = (ts) => {
+  ts.test('case: zeroes and ones', (t) => {
+    t.ok(
+      vec2.equal(
+        { x: 0, y: 0 },
+        { x: 0, y: 0 }
+      ),
+      'zero vectors equal'
+    )
+
+    t.notOk(
+      vec2.equal(
+        { x: 1, y: 1 },
+        { x: 2, y: 1 }
+      ),
+      'vectors different'
+    )
+
+    t.end()
+  })
 }
