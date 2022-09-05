146d203395eaa7125b7f80f14f86e4f19bd6a7b1
Akseli Palén <akseli.palen@gmail.com>
2022-09-05 14:39:46 +0300

    test vec2 fromPolar max min

--- test/vec2/fromPolar.test.js
+++ test/vec2/fromPolar.test.js
@@ -1,15 +1,20 @@
-module.exports = (magnitude, direction) => {
-  // Parameters
-  //   magnitude
-  //     number
-  //   direction
-  //     number, angle in radians
-  //
-  // Return
-  //   a vector
-  //
-  return {
-    x: magnitude * Math.cos(direction),
-    y: magnitude * Math.sin(direction)
-  }
+const affineplane = require('../../index')
+const vec2 = affineplane.vec2
+
+module.exports = (ts) => {
+  ts.test('case: basic fromPolar', (t) => {
+    t.almostEqual(
+      vec2.fromPolar(2, 0),
+      { x: 2, y: 0 },
+      'zero angle'
+    )
+
+    t.almostEqual(
+      vec2.fromPolar(2, Math.PI / 2),
+      { x: 0, y: 2 },
+      'angle of +90deg'
+    )
+
+    t.end()
+  })
 }
--- test/vec2/max.test.js
+++ test/vec2/max.test.js
@@ -1,7 +1,26 @@
-module.exports = (v, w) => {
-  // Element-wise maximum of two vectors
-  return {
-    x: Math.max(v.x, w.x),
-    y: Math.max(v.y, w.y)
-  }
+const affineplane = require('../../index')
+const vec2 = affineplane.vec2
+
+module.exports = (ts) => {
+  ts.test('case: basic elem-wise max', (t) => {
+    t.almostEqual(
+      vec2.max(
+        { x: 2, y: 0 },
+        { x: -3, y: -1 }
+      ),
+      { x: 2, y: 0 },
+      'absolute magnitude should not matter'
+    )
+
+    t.almostEqual(
+      vec2.max(
+        { x: 2, y: -2 },
+        { x: -2, y: 2 }
+      ),
+      { x: 2, y: 2 },
+      'mixed'
+    )
+
+    t.end()
+  })
 }
