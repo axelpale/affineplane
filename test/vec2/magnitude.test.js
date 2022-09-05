5e598450eeabf62a34a1d79c48c5feff629d7bcc
Akseli Palén <akseli.palen@gmail.com>
2022-09-05 14:20:07 +0300

    test vec2 difference, magnitude

--- test/vec2/magnitude.test.js
+++ test/vec2/magnitude.test.js
@@ -1,4 +1,20 @@
-module.exports = (v) => {
-  // Return float
-  return Math.sqrt(v.x * v.x + v.y * v.y)
+const affineplane = require('../../index')
+const vec2 = affineplane.vec2
+
+module.exports = (ts) => {
+  ts.test('case: basic magnitude', (t) => {
+    t.equal(
+      vec2.magnitude({ x: 0, y: 0 }),
+      0,
+      'zero vector'
+    )
+
+    t.almostEqual(
+      vec2.magnitude({ x: 3, y: 4 }),
+      5,
+      'simple magnitude'
+    )
+
+    t.end()
+  })
 }
