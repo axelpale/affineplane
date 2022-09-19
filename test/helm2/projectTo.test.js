0d08dd18a2b03d3293d53ce81242f8b737940119
Akseli Palen <akseli.palen@gmail.com>
2022-09-19 05:12:25 +0300

    bugfix helm2.projectTo perspective

--- test/helm2/projectTo.test.js
+++ test/helm2/projectTo.test.js
@@ -22,4 +22,36 @@ module.exports = (ts) => {
 
     t.end()
   })
+
+  ts.test('case: basic perspective projection to plane', (t) => {
+    const plane = { a: 1, b: 0, x: 0, y: 0, z: 0 }
+    const camera = { x: 1, y: 1, z: 0 }
+    const htr = { a: 1, b: 0, x: 2, y: 0 }
+    t.deepEqual(
+      helm2.projectTo(htr, plane, camera),
+      { a: 1, b: 0, x: 0, y: 0 },
+      'camera at geometry depth thus tr becomes infinite, singular'
+    )
+
+    const planeb = { a: 1, b: 0, x: 0, y: 0, z: -2 }
+    const camerab = { x: 0, y: 0, z: -4 }
+    const htrb = { a: 1, b: 0, x: 2, y: 0 }
+    t.deepEqual(
+      helm2.projectTo(htrb, planeb, camerab),
+      { a: 1, b: 0, x: 1, y: 0 },
+      'the image plane is between tr domain and camera'
+      // tr shrinks by half
+    )
+
+    const planec = { a: 3, b: 0, x: 0, y: 0, z: -2 }
+    const camerac = { x: 0, y: 0, z: -4 }
+    const htrc = { a: 2, b: 0, x: 6, y: 0 }
+    t.deepEqual(
+      helm2.projectTo(htrc, planec, camerac),
+      { a: 2, b: 0, x: 1, y: 0 },
+      'scaled image plane'
+    )
+
+    t.end()
+  })
 }
