7c742f8fbc922af0bd05a947bc8fdef4066e9e87
Akseli Palen <akseli.palen@gmail.com>
2022-10-27 17:56:18 +0300

    impl path3 transitFrom transitTo

--- test/path3/index.test.js
+++ test/path3/index.test.js
@@ -1,7 +1,9 @@
 // A unit for each method.
 const units = {
   combine: require('./combine.test'),
-  create: require('./create.test')
+  create: require('./create.test'),
+  transitFrom: require('./transitFrom.test'),
+  transitTo: require('./transitTo.test')
 }
 
 module.exports = (t) => {
---
+++ test/path3/transitFrom.test.js
@@ -0,0 +1,19 @@
+const affineplane = require('../../index')
+const path3 = affineplane.path3
+
+module.exports = (ts) => {
+  ts.test('case: basic transitFrom', (t) => {
+    // let path, plane
+
+    const path = [{ x: 0, y: 0, z: 0 }, { x: 1, y: 1, z: 1 }]
+    const plane = { a: 2, b: 0, x: 0, y: 2, z: 0 }
+
+    t.deepEqual(
+      path3.transitFrom(path, plane),
+      [{ x: 0, y: 2, z: 0 }, { x: 2, y: 4, z: 2 }],
+      'scale and translation should affect'
+    )
+
+    t.end()
+  })
+}
