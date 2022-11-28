210e80d92c5fb1e49521b157e8e3a04c317b6a3b
Akseli Palen <akseli.palen@gmail.com>
2022-11-28 23:24:45 +0200

    impl plane3.transform with origin parameter

--- test/plane3/transform.test.js
+++ test/plane3/transform.test.js
@@ -19,4 +19,34 @@ module.exports = (ts) => {
 
     t.end()
   })
+
+  ts.test('case: transform about origin', (t) => {
+    const plane = { a: 1, b: 0, x: 0, y: 0, z: 0 }
+    let tr = { a: 0, b: 1, x: 0, y: 0, z: 0 }
+    let origin = { x: 1, y: 0, z: 0 }
+
+    t.deepEqual(
+      plane3.transform(plane, tr, origin),
+      { a: 0, b: 1, x: 1, y: -1, z: 0 },
+      'rotate around non-zero origin'
+    )
+
+    // Add some translation
+    tr = { a: 0, b: 1, x: 2, y: -2, z: 2 }
+    t.deepEqual(
+      plane3.transform(plane, tr, origin),
+      { a: 0, b: 1, x: 3, y: -3, z: 2 },
+      'translate and rotate around non-zero origin'
+    )
+
+    tr = { a: 2, b: 0, x: 0, y: 0, z: 0 }
+    origin = { x: 1, y: 1, z: 1 }
+    t.deepEqual(
+      plane3.transform(plane, tr, origin),
+      { a: 2, b: 0, x: -1, y: -1, z: -1 },
+      'dilation about off-plane origin'
+    )
+
+    t.end()
+  })
 }
