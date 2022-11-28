7eeda438eae6076c8d43c683ac49a5693eb5888f
Akseli Palen <akseli.palen@gmail.com>
2022-11-28 20:10:19 +0200

    impl plane2.transform with origin parameter

--- test/plane2/transform.test.js
+++ test/plane2/transform.test.js
@@ -13,4 +13,26 @@ module.exports = (ts) => {
 
     t.end()
   })
+
+  ts.test('case: transform about origin', (t) => {
+    const plane = { a: 1, b: 0, x: 0, y: 0 }
+    let tr = { a: 0, b: 1, x: 0, y: 0 }
+    const origin = { x: 1, y: 0 }
+
+    t.deepEqual(
+      plane2.transform(plane, tr, origin),
+      { a: 0, b: 1, x: 1, y: -1 },
+      'rotate around non-zero origin'
+    )
+
+    // Add some translation
+    tr = { a: 0, b: 1, x: 2, y: -2 }
+    t.deepEqual(
+      plane2.transform(plane, tr, origin),
+      { a: 0, b: 1, x: 3, y: -3 },
+      'translate and rotate around non-zero origin'
+    )
+
+    t.end()
+  })
 }
