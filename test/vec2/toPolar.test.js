464badcb07cd174b615c91dede7ba6afcc1ec990
Akseli Palen <akseli.palen@gmail.com>
2022-11-28 17:09:39 +0200

    correct terminology in vec2 vec3 fromPolar toPolar: radius vs magnitude

--- test/vec2/toPolar.test.js
+++ test/vec2/toPolar.test.js
@@ -6,7 +6,8 @@ module.exports = (ts) => {
     t.deepEqual(
       vec2.toPolar({ x: 0, y: 0 }),
       {
-        magnitude: 0,
+        radius: 0,
+        magnitude: 0, // TODO remove in v3
         direction: 0
       }
     )
@@ -18,7 +19,8 @@ module.exports = (ts) => {
     t.deepEqual(
       vec2.toPolar({ x: 1, y: 0 }),
       {
-        magnitude: 1,
+        radius: 1,
+        magnitude: 1, // TODO remove in v3
         direction: 0
       }
     )
@@ -26,7 +28,8 @@ module.exports = (ts) => {
     t.deepEqual(
       vec2.toPolar({ x: 0, y: 1 }),
       {
-        magnitude: 1,
+        radius: 1,
+        magnitude: 1, // TODO remove in v3
         direction: Math.PI / 2
       }
     )
