334bbdc4b43c4667d54852de1b49b80a9edeb986
Akseli Palen <akseli.palen@gmail.com>
2023-01-30 16:25:10 +0200

    impl size3 .scaleBy

--- test/size3/index.test.js
+++ test/size3/index.test.js
@@ -2,6 +2,7 @@
 const units = {
   atNorm: require('./atNorm.test'),
   create: require('./create.test'),
+  scaleBy: require('./scaleBy.test'),
   transitFrom: require('./transitFrom.test'),
   transitTo: require('./transitTo.test'),
   validate: require('./validate.test'),
