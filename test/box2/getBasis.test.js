780c7151fb81579b2babd0b06c96f59d2f298ecf
Akseli Palen <akseli.palen@gmail.com>
2023-01-30 17:36:13 +0200

    rename box2.getPlane to .getBasis

--- test/box2/getBasis.test.js
+++ test/box2/getBasis.test.js
@@ -2,17 +2,17 @@ const affineplane = require('../../index')
 const box2 = affineplane.box2
 
 module.exports = (ts) => {
-  ts.test('case: basic getPath', (t) => {
+  ts.test('case: basic getBasis', (t) => {
     const b = { a: 1, b: 0, x: 1, y: 2, w: 3, h: 4 }
     t.deepEqual(
-      box2.getPlane(b),
+      box2.getBasis(b),
       { a: 1, b: 0, x: 1, y: 2 },
       'should be a plane'
     )
 
     const bb = { a: 0, b: 1, x: 1, y: 2, w: 3, h: 4 }
     t.deepEqual(
-      box2.getPlane(bb),
+      box2.getBasis(bb),
       { a: 0, b: 1, x: 1, y: 2 },
       'rotated plane'
     )
