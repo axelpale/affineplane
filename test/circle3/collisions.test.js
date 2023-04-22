24b04630de1c80772d729f0c256050dc0120774b
Akseli Palen <akseli.palen@gmail.com>
2023-04-22 17:08:23 +0300

    implement circle3.collideSegment

--- test/circle3/collisions.test.js
+++ test/circle3/collisions.test.js
@@ -51,4 +51,45 @@ module.exports = (ts) => {
 
     t.end()
   })
+
+  ts.test('case: circle-segment collision', (t) => {
+    let c, seg
+
+    c = { x: 0, y: 0, z: 0, r: 0 }
+    seg = [{ x: 0, y: 0, z: 0 }, { x: 0, y: 0, z: 0 }]
+    t.true(
+      circle3.collideSegment(c, seg),
+      'zero should collide if at the same point'
+    )
+
+    c = { x: 0, y: 0, z: 0, r: 0 }
+    seg = [{ x: 0, y: 0, z: -1 }, { x: 0, y: 0, z: 1 }]
+    t.true(
+      circle3.collideSegment(c, seg),
+      'should collide if segment goes through pole'
+    )
+
+    c = { x: 1, y: 0, z: 0, r: 0 }
+    seg = [{ x: 0, y: 0, z: -1 }, { x: 0, y: 0, z: 1 }]
+    t.false(
+      circle3.collideSegment(c, seg),
+      'should not collide if segment off'
+    )
+
+    c = { x: 0, y: 0, z: 0, r: 1 }
+    seg = [{ x: 0, y: 0, z: -1 }, { x: 0, y: 0, z: -2 }]
+    t.false(
+      circle3.collideSegment(c, seg),
+      'should not collide if segment outside'
+    )
+
+    c = { x: 0, y: 0, z: 0, r: 1 }
+    seg = [{ x: 1, y: 0, z: -1 }, { x: 1, y: 0, z: 1 }]
+    t.true(
+      circle3.collideSegment(c, seg),
+      'should collide if segment intersects circle edge'
+    )
+
+    t.end()
+  })
 }
