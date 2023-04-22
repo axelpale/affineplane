01b522d81d230e0b84b321622ad5948f92da8b57
Akseli Palen <akseli.palen@gmail.com>
2023-04-22 16:55:25 +0300

    move misplaced test suite

--- test/circle3/transformations.test.js
+++ test/circle3/transformations.test.js
@@ -49,6 +49,52 @@ module.exports = (ts) => {
     t.end()
   })
 
+  ts.test('case: basic homothety', (t) => {
+    let circle, point
+
+    circle = { x: 0, y: 0, z: 0, r: 0 }
+    point = { x: 0, y: 0, z: 0 }
+    t.deepEqual(
+      circle3.homothety(circle, point, 1),
+      { x: 0, y: 0, z: 0, r: 0 },
+      'origin at circle center'
+    )
+
+    circle = { x: 200, y: 200, z: 200, r: 100 }
+    point = { x: 0, y: 0, z: 0 }
+    t.deepEqual(
+      circle3.homothety(circle, point, 0),
+      { x: 0, y: 0, z: 0, r: 0 },
+      'zero factor towards zero'
+    )
+
+    circle = { x: 200, y: 200, z: 200, r: 100 }
+    point = { x: 1, y: 1, z: 1 }
+    t.deepEqual(
+      circle3.homothety(circle, point, 0),
+      { x: 1, y: 1, z: 1, r: 0 },
+      'zero factor towards non-zero'
+    )
+
+    circle = { x: 2, y: -2, z: 2, r: 2 }
+    point = { x: 0, y: 0, z: 0 }
+    t.deepEqual(
+      circle3.scaleBy(circle, point, 0.5),
+      { x: 1, y: -1, z: 1, r: 1 },
+      'half, alias'
+    )
+
+    circle = { x: 2, y: 2, z: 2, r: 2 }
+    point = { x: 2, y: 0, z: 0 }
+    t.deepEqual(
+      circle3.scaleBy(circle, point, 0.5),
+      { x: 2, y: 1, z: 1, r: 1 },
+      'half along y axis, alias'
+    )
+
+    t.end()
+  })
+
   ts.test('case: basic rotate by z', (t) => {
     let c, origin
     const PI = Math.PI
