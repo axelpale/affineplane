7fe1ebadb4c017cd0e2d9d289da77d486f11e546
Akseli Palen <akseli.palen@gmail.com>
2023-04-22 14:48:59 +0300

    implement circle3.homothety .scaleBy

--- test/circle3/collisions.test.js
+++ test/circle3/collisions.test.js
@@ -9,7 +9,7 @@ module.exports = (ts) => {
 
     t.false(
       circle3.hasPoint({ x: 1, y: 0, z: 0, r: 0 }, { x: 0, y: 0, z: 0 }),
-      'offset zero sphere does not collide'
+      'offset zero circle does not collide'
     )
 
     t.true(
@@ -47,6 +47,52 @@ module.exports = (ts) => {
     t.true(
       circle3.collideCircle(cc, c),
       'edges touch'
+    )
+
+    t.end()
+  })
+
+  ts.test('case: basic homothety', (t) => {
+    let circle, point
+
+    circle = { x: 0, y: 0, z: 0, r: 0 }
+    point = { x: 0, y: 0, z: 0 }
+    t.deepEqual(
+      circle3.homothety(circle, point, 1),
+      { x: 0, y: 0, z: 0, r: 0 },
+      'origin at circle center'
+    )
+
+    circle = { x: 200, y: 200, z: 200, r: 100 }
+    point = { x: 0, y: 0, z: 0 }
+    t.deepEqual(
+      circle3.homothety(circle, point, 0),
+      { x: 0, y: 0, z: 0, r: 0 },
+      'zero factor towards zero'
+    )
+
+    circle = { x: 200, y: 200, z: 200, r: 100 }
+    point = { x: 1, y: 1, z: 1 }
+    t.deepEqual(
+      circle3.homothety(circle, point, 0),
+      { x: 1, y: 1, z: 1, r: 0 },
+      'zero factor towards non-zero'
+    )
+
+    circle = { x: 2, y: -2, z: 2, r: 2 }
+    point = { x: 0, y: 0, z: 0 }
+    t.deepEqual(
+      circle3.scaleBy(circle, point, 0.5),
+      { x: 1, y: -1, z: 1, r: 1 },
+      'half, alias'
+    )
+
+    circle = { x: 2, y: 2, z: 2, r: 2 }
+    point = { x: 2, y: 0, z: 0 }
+    t.deepEqual(
+      circle3.scaleBy(circle, point, 0.5),
+      { x: 2, y: 1, z: 1, r: 1 },
+      'half along y axis, alias'
     )
 
     t.end()
