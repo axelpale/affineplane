dfc23841e916a8ff0d780cc8e6fdaf7c07569418
Akseli Palen <akseli.palen@gmail.com>
2023-04-22 14:01:31 +0300

    implement circle3.polarOffset

--- test/circle3/transformations.test.js
+++ test/circle3/transformations.test.js
@@ -32,4 +32,20 @@ module.exports = (ts) => {
 
     t.end()
   })
+
+  ts.test('case: basic polar offset', (t) => {
+    t.deepEqual(
+      circle3.polarOffset({ x: 0, y: 0, z: 0, r: 1 }, 1, 0),
+      { x: 1, y: 0, z: 0, r: 1 },
+      'should move towards x'
+    )
+
+    t.deepEqual(
+      circle3.polarOffset({ x: 3, y: -3, z: 3, r: 1 }, 1, 2, 0),
+      { x: 3, y: -3, z: 4, r: 1 },
+      'should move towards z, theta does not matter'
+    )
+
+    t.end()
+  })
 }
