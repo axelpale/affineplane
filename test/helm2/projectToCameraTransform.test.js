1f25c1171e98bb08abe23ae2802a29259e716e11
Akseli Palen <akseli.palen@gmail.com>
2023-04-26 21:20:52 +0300

    remove unused view plane parameter

--- test/helm2/projectToCameraTransform.test.js
+++ test/helm2/projectToCameraTransform.test.js
@@ -2,41 +2,38 @@ const helm2 = require('../../lib/helm2')
 
 module.exports = (ts) => {
   ts.test('case: project to camera transform', (t) => {
-    let tr, origin, plane, camera
+    let tr, origin, camera
 
     tr = { a: 1, b: 0, x: 0, y: 0 }
     origin = { x: 0, y: 0, z: 0 }
-    plane = { a: 1, b: 0, x: 0, y: 0, z: 0 }
     camera = { x: 0, y: 0, z: -1 }
     t.almostEqualHelmert(
-      helm2.projectToCameraTransform(tr, origin, plane, camera),
+      helm2.projectToCameraTransform(tr, origin, camera),
       { a: 1, b: 0, x: 0, y: 0, z: 0 },
       'trivial noop projection'
     )
 
     tr = { a: 2, b: 0, x: 0, y: 0 }
     origin = { x: 0, y: 0, z: 0 }
-    plane = { a: 1, b: 0, x: 0, y: 0, z: 0 }
     camera = { x: 0, y: 0, z: -1 }
     t.almostEqualHelmert(
-      helm2.projectToCameraTransform(tr, origin, plane, camera),
+      helm2.projectToCameraTransform(tr, origin, camera),
       { a: 1, b: 0, x: 0, y: 0, z: 0.5 },
       'transform on the plane, travel a bit closer'
     )
 
     tr = { a: 0, b: 2, x: 2, y: 2 }
     origin = { x: 0, y: 0, z: 2 }
-    plane = { a: 1, b: 0, x: 0, y: 0, z: 1 }
     camera = { x: 0, y: 0, z: 0 }
     t.almostEqualHelmert(
-      helm2.projectToCameraTransform(tr, origin, plane, camera),
+      helm2.projectToCameraTransform(tr, origin, camera),
       { a: 0, b: -1, x: -1, y: -1, z: 1 },
       'should invert rotation and invert and scale translation'
     )
 
     tr = { a: 0, b: 0, x: 2, y: 2 }
     t.almostEqualHelmert(
-      helm2.projectToCameraTransform(tr, origin, plane, camera),
+      helm2.projectToCameraTransform(tr, origin, camera),
       { a: 1, b: 0, x: 0, y: 0, z: 0 },
       'should handle singular helmert'
     )
