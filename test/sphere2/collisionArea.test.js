dda2abca5da967a9051cddc932587c397cf15840
Akseli Palen <akseli.palen@gmail.com>
2023-10-16 00:27:39 +0300

    repair sphere2.collisionArea special case

--- test/sphere2/collisionArea.test.js
+++ test/sphere2/collisionArea.test.js
@@ -1,4 +1,6 @@
 const sphere2 = require('../../lib/sphere2')
+const sqrt = Math.sqrt
+const acos = Math.acos
 
 module.exports = (ts) => {
   ts.test('case: basic collisionArea', (t) => {
@@ -25,7 +27,21 @@ module.exports = (ts) => {
       // overlap = 2 * segment
       // segment = π/3 - sqrt(3)/4
       (2 * Math.PI / 3) - Math.sqrt(3) / 2,
-      'spheres overlap'
+      'spheres overlap, sector angle < 180'
+    )
+
+    t.almostEqual(
+      sphere2.collisionArea({ x: 0, y: 0, r: 3 }, { x: 2, y: 0, r: 2 }),
+      // overlap = small circle - small circle segment + large circle segment
+      4 * Math.PI + 9 * acos(3 / 4) - 4 * acos(1 / 8) - 3 * sqrt(7) / 2,
+      'spheres overlap, sector angle > 180'
+    )
+
+    t.almostEqual(
+      sphere2.collisionArea({ x: 967, y: 485, r: 212 }, { x: 667, y: 485, r: 825 }),
+      // overlap = small circle
+      Math.PI * 212 * 212,
+      'real world numbers'
     )
 
     t.end()
